b8260979420d7db1ce6801bb48a5fca421ff0c2c
vitalspace <myglobalsounds@gmail.com>
2023-08-19 20:34:08 -0600

    added final comments

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+// Get DOM element by selector
 const elem = (target) => document.querySelector(target);
+
+// Component to render a store
 const StoreComponet = (store) => {
+  // Set CSS class based on open/closed
   const openClass = store.isOpen === "open" ? "open" : "closed";
+  // Return store HTML
   return `<div class="store">
     <div>
       <h2>${store.name}</h2>
@@ -15,6 +20,7 @@ const StoreComponet = (store) => {
   `;
 };
 
+// Fetch JSON data from file
 const openJson = async (fileName) => {
   try {
     const req = await fetch(fileName);
@@ -24,22 +30,29 @@ const openJson = async (fileName) => {
   }
 };
 
+// Render all stores to DOM
 const renderStores = (stores) => {
+  // Clear input and list
   elem("#zip-code").value = "";
   elem("#store-list").innerHTML = "";
+  // Loop stores and render
   stores.map((store) => {
     elem("#store-list").innerHTML += StoreComponet(store);
   });
 };
 
+// Find store by zip code
 const findStore = (stores, zip) => stores.find((s) => s.zip.includes(zip));
 
+// Handle zip code search
 const handleZipSearch = (e, stores) => {
+  // If input empty, render all
   if (e.target.value === "") {
     renderStores(stores);
     return;
   }
 
+  // Clear list, find store, render or show not found
   elem("#store-list").innerHTML = "";
 
   const store = findStore(stores, e.target.value);
@@ -49,9 +62,12 @@ const handleZipSearch = (e, stores) => {
     : (elem("#store-list").innerHTML = "No store found");
 };
 
+// App entry point
 const main = async () => {
+  // Get stores
   const stores = await openJson("stores.json");
 
+  // Bind events
   elem("#zip-code").addEventListener("keydown", (e) =>
     handleZipSearch(e, stores)
   );
@@ -59,7 +75,8 @@ const main = async () => {
   elem("#view-all-stores").addEventListener("click", (e) =>
     renderStores(stores)
   );
-
+  
+  // Initial render
   renderStores(stores);
 };
 
